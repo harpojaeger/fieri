1b825d5d90a80ce99e21a71ce5d0f5be07f6c7c9
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-01-06 13:55:17 -0500

    output dishes

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <html>
 <head>
   <title>Today's Menu</title>
+  <style>
+  div.dishName {
+    font-weight:bold;
+  }
+  div.dishDescription {
+    font-style: italic;
+  }
+  ul.dishList {
+    list-style-type: none;
+  }
+  </style>
 </head>
 <body>
   <?php
@@ -10,6 +21,46 @@
   $dotenv = new Dotenv\Dotenv(__DIR__);
   $dotenv->load();
 
+  // Define a class to handle formatting the output for each dish
+  class Dish {
+    function __construct($id, $name, $description){
+      // if(!$id && !$name && !$description) die('Dish requires id, name & description.')
+      $this->id = $id;
+      $this->name = $name;
+      $this->description = $description;
+    }
+    function html(){
+      return "<div class='dishName'>$this->name</div><div class='dishDescription'>$this->description</div>";
+    }
+  }
+
+  // Open up a new db connection
+  $conn = new mysqli($_ENV['MYSQL_HOST'], $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASS'], $_ENV['MYSQL_DB']);
+  if ($conn->connect_error) die('Connection error: ' . $conn->connect_error);
+
+  // Get lunch & dinner dishes
+  $lunch = $conn->query("SELECT dishes.name AS dish_name, dishes.description AS dish_description, dishes.id AS dish_id FROM dishes dishes INNER JOIN meals AS meals ON dishes.id=meals.dish_id WHERE meals.name='lunch'");
+
+  $dinner = $conn->query("SELECT dishes.name AS dish_name, dishes.description AS dish_description, dishes.id AS dish_id FROM dishes dishes INNER JOIN meals AS meals ON dishes.id=meals.dish_id WHERE meals.name='dinner'");
+
+  echo('<h2>Lunch</h2><ul class="dishList">');
+  while($this_dish = $lunch->fetch_assoc()){
+    $dish = new Dish($this_dish['dish_id'],$this_dish['dish_name'],$this_dish['dish_description']);
+
+    echo("<li>".$dish->html()."</li>");
+  }
+
+  echo('</ul><h2>Dinner</h2><ul class="dishList">');
+  while($this_dish = $dinner->fetch_assoc()){
+    $dish = new Dish($this_dish['dish_id'],$this_dish['dish_name'],$this_dish['dish_description']);
+
+    echo("<li>".$dish->html()."</li>");
+  }
+  echo("</ul>");
+
+
+  // Close the db connection
+  mysqli_close($conn)
   ?>
 
 </body>
